49f141c512984da9406a4aa2a9fdef155b98a094
guzell <guzel.marselevna@gmail.com>
2016-04-08 13:05:23 +0400

    add js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /**
  * Created by Guzel on 07.04.2016.
  */
-//$(function (){
-//    $('#rightbar').height($('.box').height());
-//});
+$(function (){
+    $('#rightbar').height($('.box').height());
+});
